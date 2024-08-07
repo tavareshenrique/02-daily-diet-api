8c73c6fa606af2dd23f6b50227ee9d2761875734
Henrique Tavares <ihenrits@gmail.com>
2024-08-07 07:06:04 -0300

    feat: Register mealsRoutes for handling meal-related functionality
    The code changes in this commit import and register the `mealsRoutes` in the `app.ts` file. This allows the application to handle meal-related functionality by routing requests to the appropriate endpoints.
    
    Recent user commits:
    - feat: Fix typo in variable name for user existence check
    - feat: Add checkSessionIdExists middleware
    - feat: Email already use validation
    - feat: Add user registration functionality
    - feat: Add password field to 'users' table in knex.d.ts
    - feat: Add migration to create user password column
    - feat: Add type declaration for 'users' table in knex.d.ts
    
    Recent repository commits:
    - feat: Update app.ts to register usersRoutes for user-related functionality
    - feat: Add migration to create users table
    - chore: Add knexfile.ts for database configuration
    - chore: Add initial project files and configurations

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 
+import { mealsRoutes } from './routes/meals'
 import { usersRoutes } from './routes/users'
 
 export const app = fastify()
@@ -9,4 +10,8 @@ app.register(cookie)
 
 app.register(usersRoutes, {
   prefix: 'users',
+})
+
+app.register(mealsRoutes, {
+  prefix: 'meals',
 })
